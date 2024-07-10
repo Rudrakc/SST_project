b1aebc9492e24b1a1c65cb2eb3cf6c0a42476386
Rudrakc <rudrachauhan2888@gmail.com>
2024-07-10 10:55:37 +0530

    Added User registration

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,11 +1,26 @@
 const express = require("express");
 const User = require("../models/userModel");
+const bcrypt = require('bcrypt')
 
 const router = express.Router();
 
 router.post("/register", async (req, res) => {
+    try {
+        const UserExists = await User.findOne({ email: req.body.email });
+        if (UserExists) {
+            return res.status(400).json({ message: "User already exists" });
+        }
 
-
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(req.body.password, salt);
+        req.body.password = hashedPassword;
+        
+        const newUser = new User(req.body);
+        await newUser.save();
+        res.status(200).json(newUser);
+    } catch (error) {
+        
+    }
 });
 
 router.post("/login", async (req, res) => {
